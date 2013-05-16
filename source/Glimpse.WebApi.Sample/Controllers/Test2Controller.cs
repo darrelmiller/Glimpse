e0a75663222da845a7bc1c7a829670a15971e4d7
Darrel <darrel@tavis.ca>
2013-05-15 21:41:16 -0400

    The files the last commit ignored

---
+++ source/Glimpse.WebApi.Sample/Controllers/Test2Controller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Glimpse.WebApi.Sample.Controllers
+{
+    public class Test2Controller : ApiController
+    {
+        public HttpResponseMessage Get(HttpRequestMessage request)
+        {
+            return new HttpResponseMessage()
+                {
+                    Content =  new StringContent("<html><body>Hello World <a href='test'>Test</a></body></html>", Encoding.UTF8,"text/html")
+                };
+        } 
+    }
+}
